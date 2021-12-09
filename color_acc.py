3e2d5adb2abddacfa2201f0c837f631adbf90f87
Kazen Gallman <kazar444@icloud.com>
2021-12-09 16:27:31 -0500

    added function to get color accuracy

--- color_acc.py
+++ color_acc.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 colorsToNum = {
     "black":0,
@@ -18,4 +19,17 @@ colorsToNum = {
 
 def colorsAcc(probs, labels):
 
+    acc = np.zeros(12)
+
+    for i in range(0,12):
+        colorP = []
+        colorL = []
+        
+        for j, val in enumerate(labels):
+            if val == i:
+                colorP.append(probs[j])
+                colorL.append(labels[j])
+
+        acc[i] = np.mean(np.array(colorL) == np.argmax(np.array(colorP), axis = 1))
     
+    return acc
