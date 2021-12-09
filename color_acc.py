38a9169d5503260edba8ffe1e9137439d27ede0e
Kazen Gallman <kazar444@icloud.com>
2021-12-09 16:29:18 -0500

    added functions to include counts

--- color_acc.py
+++ color_acc.py
@@ -20,16 +20,21 @@ colorsToNum = {
 def colorsAcc(probs, labels):
 
     acc = np.zeros(12)
+    counts = np.zeros(12)
 
     for i in range(0,12):
         colorP = []
         colorL = []
         
+        colorC = 0
+
         for j, val in enumerate(labels):
             if val == i:
                 colorP.append(probs[j])
                 colorL.append(labels[j])
+                colorC = colorC + 1
 
         acc[i] = np.mean(np.array(colorL) == np.argmax(np.array(colorP), axis = 1))
+        counts[i] = colorC
     
-    return acc
+    return acc, counts
